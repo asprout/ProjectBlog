b9618d54b1f78fd8cfdff27ac2d4ffa2da6ee5d1
DennisNenov <nycdennen@gmail.com>
2014-10-22 20:19:33 -0400

    blogs display

--- populate.py
+++ populate.py
@@ -3,15 +3,22 @@ import sqlite3
 def create_db():
     conn = sqlite3.connect("blog.db")
     c = conn.cursor()
-    c.execute("create table posts(title text, content text, author text, blog_id integer)")
-    c.execute("create table comments(content text, blog integer, author text)")
+    c.execute("create table blog (name text, blog_id integer)")
+    c.execute("create table posts(title text, content text, author text, blog_id integer, post_id integer)")
+    c.execute("create table comments(content text, post_id integer, author text)")
     conn.commit()
     conn.close()
     
 def add_blog(name):
     conn = sqlite3.connect("blog.db")
     c = conn.cursor()
-    command = "INSERT INTO blogs VALUES(" + name + ")"
+    currentMax = c.execute("select max(blog_id) from blog").fetchone()
+    print currentMax[0]
+    if currentMax[0] == None:
+        newid = 1
+    else:
+       newid = int(currentMax[0]) + 1
+    command = "INSERT INTO blog(name, blog_id) VALUES('" + name + "'," + str(newid) +  ")"
     c.execute(command)
     conn.commit()
     conn.close()
@@ -19,9 +26,11 @@ def add_blog(name):
 def blog_list():
     conn = sqlite3.connect("blog.db")
     c = conn.cursor()
-    cur = c.execute("SELECT name FROM blogs")
     blogs = []
-    for blog in cur:
-        print blog
-        blogs.append(blog)
+    for row in c.execute("select name from blog"):
+        blogs.append(row[0])
+    print blogs
     return blogs
+
+blog_list()
+
